68a91ed7b591e9729487a0eaa2ac6c30c34a7de6
yan_h <yan_h@hotmail.com>
2018-12-03 22:11:36 +0800

    add fetcher project

---
+++ fetcher/src/main/java/com/nobodyhub/transcendence/fetcher/FetcherApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.transcendence.fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetcherApplication.class, args);
+    }
+}
