e292a3df2c4d9a63f17c30d29c994c78955bad0c
yan_h <yan_h@worksap.co.jp>
2018-12-05 19:02:02 +0800

    add module zhihu_common

---
+++ zhihu_common/src/main/java/com/nobodyhub/transcendence/zhihu/common/ZhihuCommonApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.transcendence.zhihu.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuCommonApplication.class, args);
+    }
+}
---
+++ zhihu_common/src/test/java/com/nobodyhub/transcendence/zhihu/common/ZhihuCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nobodyhub.transcendence.zhihu.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhihuCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
