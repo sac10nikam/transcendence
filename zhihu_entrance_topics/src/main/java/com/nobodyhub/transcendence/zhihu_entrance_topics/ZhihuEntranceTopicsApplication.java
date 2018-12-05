e96799c2b89cb3bae1f45a8e14497a2aaf918d14
yan_h <yan_h@worksap.co.jp>
2018-12-05 18:41:10 +0800

    add module zhihu_entrance_topics

---
+++ zhihu_entrance_topics/src/main/java/com/nobodyhub/transcendence/zhihu_entrance_topics/ZhihuEntranceTopicsApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.transcendence.zhihu_entrance_topics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuEntranceTopicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuEntranceTopicsApplication.class, args);
+    }
+}
