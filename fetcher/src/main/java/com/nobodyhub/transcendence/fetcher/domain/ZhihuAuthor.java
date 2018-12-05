5d95bc93a684806d7356bb9a6d68598d280e30f3
yan_h <yan_h@worksap.co.jp>
2018-12-05 12:48:15 +0800

    remove mongodb related

--- fetcher/src/main/java/com/nobodyhub/transcendence/fetcher/domain/ZhihuAuthor.java
+++ fetcher/src/main/java/com/nobodyhub/transcendence/fetcher/domain/ZhihuAuthor.java
@@ -1,46 +1,25 @@
 package com.nobodyhub.transcendence.fetcher.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
-@Document
 public class ZhihuAuthor {
-    @Id
     private String id;
-
-    @Indexed(unique = true)
     private String url_token;
-
     private String name;
-
     private String avatar_url;
-
     private String avatar_url_template;
-
     private Boolean is_org;
-
     private String type;
-
     private String url;
-
     private String user_type;
-
     private String headline;
-
     private int gender;
-
     private Boolean is_advertiser;
-
     private int follower_count;
-
     private int answer_count;
-
     private int articles_count;
-
     private List<ZhihuEmployment> employments;
 }
