aaac5ed06d146e16ba86a21e31a88ffde58fab4d
yan_h <yan_h@worksap.co.jp>
2018-12-07 13:12:03 +0800

    create zhihu_member module

---
+++ zhihu_member/src/main/java/com/nobodyhub/transcendence/zhihu/member/ZhihuMemberApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.transcendence.zhihu.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuMemberApplication.class, args);
+    }
+}
