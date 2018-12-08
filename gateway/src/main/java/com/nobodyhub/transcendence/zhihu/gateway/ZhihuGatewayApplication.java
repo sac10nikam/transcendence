af77fd3c4194a92020eab3679ae9202f770efd84
yan_h <yan_h@hotmail.com>
2018-12-08 23:23:30 +0800

    configure service discovery and gateway

--- gateway/src/main/java/com/nobodyhub/transcendence/zhihu/gateway/ZhihuGatewayApplication.java
+++ gateway/src/main/java/com/nobodyhub/transcendence/zhihu/gateway/ZhihuGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.nobodyhub.transcendence.zhihu.gateway;
 
+import com.nobodyhub.transcendence.zhihu.gateway.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class ZhihuGatewayApplication {
 
     public static void main(String[] args) {
