25307ef796bf3811a420b7195a5fd5c2be330a85
yan_h <yan_h@hotmail.com>
2018-12-08 16:34:24 +0800

    update zhihu_member to use eureka

--- zhihu_member/src/main/java/com/nobodyhub/transcendence/zhihu/member/ZhihuMemberApplication.java
+++ zhihu_member/src/main/java/com/nobodyhub/transcendence/zhihu/member/ZhihuMemberApplication.java
@@ -2,7 +2,9 @@ package com.nobodyhub.transcendence.zhihu.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ZhihuMemberApplication {
 
