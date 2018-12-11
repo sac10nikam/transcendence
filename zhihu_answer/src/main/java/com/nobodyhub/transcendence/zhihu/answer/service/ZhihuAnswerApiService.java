22c46272ea39d1e607b3d8cd4738032350ba661e
yan_h <yan_h@worksap.co.jp>
2018-12-11 19:09:08 +0800

    [zhihu_answer]add ZhihuAnswerApiService interface

---
+++ zhihu_answer/src/main/java/com/nobodyhub/transcendence/zhihu/answer/service/ZhihuAnswerApiService.java
@@ -0,0 +1,35 @@
+package com.nobodyhub.transcendence.zhihu.answer.service;
+
+import com.nobodyhub.transcendence.zhihu.domain.dto.ZhihuAnswer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ZhihuAnswerApiService {
+    /**
+     * Get the answer of given id
+     * http://www.zhihu.com/api/v4/answers/360364772?include=data%5B%2A%5D.is_normal%2Cis_collapsed%2Ccollapse_reason%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees&limit=100&offset=10&data%5B%2A%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics=
+     *
+     * @param answerId
+     * @return
+     */
+    Optional<ZhihuAnswer> getById(String answerId);
+
+    /**
+     * Get the answers of given member
+     * https://www.zhihu.com/api/v4/members/yun-dong-shan/answers?sort_by=default&include=data%5B%2A%5D.is_normal%2Cis_collapsed%2Ccollapse_reason%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees&limit=100&offset=10&data%5B%2A%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics=
+     *
+     * @param memberToken
+     * @return
+     */
+    List<ZhihuAnswer> getByMember(String memberToken);
+
+    /**
+     * Get the answer of given question
+     * https://www.zhihu.com/api/v4/questions/60531155/answers?offset=0&limit=62&sort_by=default
+     *
+     * @param questionId
+     * @return
+     */
+    List<ZhihuAnswer> getByQuestion(String questionId);
+}
