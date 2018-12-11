e4497a0ea7f8686943ecba53e2fa8c786b135719
yan_h <yan_h@worksap.co.jp>
2018-12-11 18:57:16 +0800

    [zhihu_answer]add new module

---
+++ zhihu_answer/src/main/java/com/nobodyhub/transcendence/zhihu/answer/ZhihuAnswerApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.transcendence.zhihu.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuAnswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuAnswerApplication.class, args);
+    }
+}
---
+++ zhihu_answer/src/test/java/com/nobodyhub/transcendence/zhihu/answer/ZhihuAnswerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nobodyhub.transcendence.zhihu.answer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhihuAnswerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
