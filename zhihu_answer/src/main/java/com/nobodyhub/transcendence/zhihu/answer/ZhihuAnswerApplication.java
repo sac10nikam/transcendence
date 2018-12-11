11dcf86390fab10a90162aae193889297b8549a9
yan_h <yan_h@worksap.co.jp>
2018-12-11 19:08:48 +0800

    [zhihu_answer]enable eureka-client

--- zhihu_answer/src/main/java/com/nobodyhub/transcendence/zhihu/answer/ZhihuAnswerApplication.java
+++ zhihu_answer/src/main/java/com/nobodyhub/transcendence/zhihu/answer/ZhihuAnswerApplication.java
@@ -2,7 +2,9 @@ package com.nobodyhub.transcendence.zhihu.answer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ZhihuAnswerApplication {
 
