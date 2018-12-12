f112e17c5ca039e01cc6d9b73a19c7e151ab55e8
yan_h <yan_h@hotmail.com>
2018-12-12 21:53:43 +0800

    [zhihu_member_api]add new module

---
+++ zhihu_member_api/src/main/java/com/nobodyhub/transcendence/zhihu/member/api/ZhihuMemberApiApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.zhihu.member.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuMemberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuMemberApiApplication.class, args);
+    }
+
+}
+
