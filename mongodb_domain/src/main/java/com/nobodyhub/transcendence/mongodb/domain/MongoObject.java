62b1f0797a7ceb6626a05641363cce0fc41b1362
yan_h <yan_h@worksap.co.jp>
2018-12-12 18:50:22 +0800

    [mongodb_domain]add abstract class to get data from MongoObject

--- mongodb_domain/src/main/java/com/nobodyhub/transcendence/mongodb/domain/MongoObject.java
+++ mongodb_domain/src/main/java/com/nobodyhub/transcendence/mongodb/domain/MongoObject.java
@@ -3,6 +3,7 @@ package com.nobodyhub.transcendence.mongodb.domain;
 import com.nobodyhub.transcendence.common.merge.Mergeable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.joda.time.DateTime;
 import org.springframework.data.annotation.Id;
@@ -18,6 +19,7 @@ public abstract class MongoObject<T extends Mergeable> {
      * mongodb object id, primary key
      */
     @Id
+    @Getter
     private ObjectId objectId;
 
     /**
@@ -30,5 +32,13 @@ public abstract class MongoObject<T extends Mergeable> {
     /**
      * The raw data fetched from API
      */
-    private T data;
+    protected T data;
+
+    /**
+     * Update the data in the object
+     * Usually contains updating of {@link #data} and other other necessary fields
+     *
+     * @param data
+     */
+    public abstract void setData(T data);
 }
