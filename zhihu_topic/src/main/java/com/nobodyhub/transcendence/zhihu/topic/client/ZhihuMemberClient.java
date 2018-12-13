75dfe304e6ff832834648fc123a81386154ed93b
yan_h <yan_h@worksap.co.jp>
2018-12-13 18:15:20 +0800

    [zhihu_topic]rename FeignClient#name for ZhihuMemberClient

--- zhihu_topic/src/main/java/com/nobodyhub/transcendence/zhihu/topic/client/ZhihuMemberClient.java
+++ zhihu_topic/src/main/java/com/nobodyhub/transcendence/zhihu/topic/client/ZhihuMemberClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "zhihu-member", path = "/members")
+@FeignClient(name = "zhihu_member", path = "/members")
 public interface ZhihuMemberClient {
     @PostMapping
     ZhihuApiMember save(@RequestBody ZhihuApiMember member);
