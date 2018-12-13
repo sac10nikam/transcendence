86554f39def7800af63ee457a112cb956a6a2f73
yan_h <yan_h@worksap.co.jp>
2018-12-13 17:09:21 +0800

    [api_throttle]create module

---
+++ api_throttle/src/main/java/com/nobodyhub/transcendence/api/throttle/ApiThrottleApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.api.throttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiThrottleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiThrottleApplication.class, args);
+    }
+
+}
+
