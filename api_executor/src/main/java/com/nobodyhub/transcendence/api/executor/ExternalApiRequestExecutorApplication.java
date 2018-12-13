30554a5f8277e1f12443d0cf38f65339df64a17c
yan_h <yan_h@worksap.co.jp>
2018-12-13 18:50:30 +0800

    [api_executor]create new module

---
+++ api_executor/src/main/java/com/nobodyhub/transcendence/api/executor/ExternalApiRequestExecutorApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.api.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiRequestExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApiRequestExecutorApplication.class, args);
+    }
+
+}
+
---
+++ api_executor/src/test/java/com/nobodyhub/transcendence/api/executor/ExternalApiRequestExecutorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nobodyhub.transcendence.api.executor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExternalApiRequestExecutorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
