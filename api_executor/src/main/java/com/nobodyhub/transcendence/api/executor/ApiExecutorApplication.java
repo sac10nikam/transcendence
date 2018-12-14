0e1a2b0503d22128a591a973a88893246dea673b
yan_h <yan_h@worksap.co.jp>
2018-12-14 18:11:52 +0800

    [api_executor]enable Eureka client

--- api_executor/src/main/java/com/nobodyhub/transcendence/api/executor/ApiExecutorApplication.java
+++ api_executor/src/main/java/com/nobodyhub/transcendence/api/executor/ApiExecutorApplication.java
@@ -2,14 +2,16 @@ package com.nobodyhub.transcendence.api.executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-public class ExternalApiRequestExecutorApplication {
+public class ApiExecutorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExternalApiRequestExecutorApplication.class, args);
+        SpringApplication.run(ApiExecutorApplication.class, args);
     }
 
 }
