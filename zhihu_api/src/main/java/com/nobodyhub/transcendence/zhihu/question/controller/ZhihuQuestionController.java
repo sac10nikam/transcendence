4be4f89293186561cc9fb07b04fa328c2197911f
yan_h <yan_h@worksap.co.jp>
2018-12-24 11:41:46 +0800

    [zhihu_api]remove unnecessary endpoint in ZhihuQuestion

--- zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/question/controller/ZhihuQuestionController.java
+++ zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/question/controller/ZhihuQuestionController.java
@@ -2,7 +2,10 @@ package com.nobodyhub.transcendence.zhihu.question.controller;
 
 import com.nobodyhub.transcendence.zhihu.api.domain.ZhihuApiQuestion;
 import com.nobodyhub.transcendence.zhihu.question.service.ZhihuQuestionService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/questions")
@@ -16,10 +19,5 @@ public class ZhihuQuestionController {
     @GetMapping("/id/{id}")
     ZhihuApiQuestion findById(@PathVariable("id") String id) {
         return zhihuQuestionService.findById(id).orElse(new ZhihuApiQuestion());
-    }
-
-    @PostMapping
-    ZhihuApiQuestion save(@RequestBody ZhihuApiQuestion question) {
-        return zhihuQuestionService.save(question);
     }
 }
