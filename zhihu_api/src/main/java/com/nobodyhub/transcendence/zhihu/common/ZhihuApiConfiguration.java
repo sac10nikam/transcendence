366c78f9fd1d3243108779dd31f15401e7f59200
yan_h <yan_h@worksap.co.jp>
2018-12-24 12:04:05 +0800

    [zhihu_api]update ZhihuUrlConverter

--- zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/common/ZhihuApiConfiguration.java
+++ zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/common/ZhihuApiConfiguration.java
@@ -1,7 +1,5 @@
 package com.nobodyhub.transcendence.zhihu.common;
 
-import com.nobodyhub.transcendence.zhihu.common.converter.ZhihuUrlConverter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +10,6 @@ public class ZhihuApiConfiguration {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.rootUri("https://www.zhihu.com").build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "zhihu.api.version", havingValue = "4")
-    public ZhihuUrlConverter converter() {
-        return url -> {
-            if (url == null) {
-                return null;
-            }
-            return url.replaceFirst("^(https://|http://)", "") // remove protocol
-                .replaceFirst("^www\\.zhihu\\.com", ""); // remove host
-        };
+        return builder.rootUri("https://www.zhihu.com/api").build();
     }
 }
--- zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/common/converter/ZhihuUrlConverter.java
+++ zhihu_api/src/main/java/com/nobodyhub/transcendence/zhihu/common/converter/ZhihuUrlConverter.java
@@ -1,11 +1,39 @@
 package com.nobodyhub.transcendence.zhihu.common.converter;
 
-public interface ZhihuUrlConverter {
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@ConditionalOnProperty(name = "zhihu.api.version", havingValue = "4")
+public class ZhihuUrlConverter {
+    private final RestTemplate restTemplate;
+
+    public ZhihuUrlConverter(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
     /**
      * Convert given URL to a compatible url with current API version
      *
      * @param url
      * @return
      */
-    String convert(String url);
+    public String convert(String url) {
+        if (url == null) {
+            return null;
+        }
+        return url.replaceFirst("^(https://|http://)", "") // remove protocol
+            .replaceFirst("^www\\.zhihu\\.com", ""); // remove host
+    }
+
+    /**
+     * Expand the given URI template with an array of URI variables.
+     *
+     * @param uriTemplate
+     * @param uriVariables
+     * @return
+     */
+    public String expand(String uriTemplate, Object... uriVariables) {
+        return this.restTemplate.getUriTemplateHandler().expand(uriTemplate, uriVariables).toString();
+    }
 }
