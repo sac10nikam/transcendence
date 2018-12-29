7ce2cf1d3408b140587b37a9145b5369c54b050a
yan_h <yan_h@hotmail.com>
2018-12-29 17:09:06 +0800

    [api_common]add handler to parse KafkaHeader

---
+++ api_common/src/main/java/com/nobodyhub/transcendence/api/common/kafka/KafkaHeaderHandler.java
@@ -0,0 +1,72 @@
+package com.nobodyhub.transcendence.api.common.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class KafkaHeaderHandler {
+    private final ObjectMapper objectMapper;
+
+    public KafkaHeaderHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * Parse content from message header to given type
+     *
+     * @param messageHeaders kafka message headers
+     * @param header         the key to be parse in the header
+     * @param <T>
+     * @return
+     */
+    public <T> Optional<T> get(@NotNull MessageHeaders messageHeaders, @NotNull KafkaMessageHeader<T> header) {
+        Assert.notNull(messageHeaders, "Message headers can not be null!");
+        Assert.notNull(header, "The header can not be null!");
+        String headerStr = messageHeaders.get(header.getKey(), String.class);
+        if (headerStr != null) {
+            try {
+                return Optional.of(objectMapper.readValue(headerStr, header.getType()));
+            } catch (IOException e) {
+                log.error("Fail to deserialize {} into {}.", headerStr, header.getType().getName());
+            }
+        }
+        return Optional.empty();
+    }
+
+    public KafkaHeaderBuilder builder() {
+        return new KafkaHeaderBuilder(this.objectMapper);
+    }
+
+    @RequiredArgsConstructor
+    public static class KafkaHeaderBuilder {
+        private final Map<String, Object> headers = Maps.newHashMap();
+        private final ObjectMapper objectMapper;
+
+        public KafkaHeaderBuilder put(@NotNull String key, Object object) {
+            try {
+                headers.put(key, objectMapper.writeValueAsString(object));
+            } catch (JsonProcessingException e) {
+                log.error("Fail to serialize {}.", object);
+            }
+            return this;
+        }
+
+        public MessageHeaders build() {
+            return new MessageHeaders(headers);
+        }
+    }
+
+
+}
---
+++ api_common/src/main/java/com/nobodyhub/transcendence/api/common/kafka/KafkaMessageHeader.java
@@ -0,0 +1,35 @@
+package com.nobodyhub.transcendence.api.common.kafka;
+
+import com.nobodyhub.transcendence.api.common.message.ApiRequestMessage;
+import lombok.Getter;
+import org.springframework.http.HttpHeaders;
+
+public class KafkaMessageHeader<T> {
+    /**
+     * The original request that get the contents of message
+     */
+    public static final KafkaMessageHeader<ApiRequestMessage> ORIGIN_REQUEST
+        = new KafkaMessageHeader<>("origin-request", ApiRequestMessage.class);
+
+    /**
+     * the header of the response
+     */
+    public static final KafkaMessageHeader<HttpHeaders> RESPONSE_HEADERS
+        = new KafkaMessageHeader<>("response-headers", HttpHeaders.class);
+
+    /**
+     * key in the message header
+     */
+    @Getter
+    private String key;
+    /**
+     * source type before serialization
+     */
+    @Getter
+    private Class<T> type;
+
+    private KafkaMessageHeader(String key, Class<T> type) {
+        this.key = key;
+        this.type = type;
+    }
+}
