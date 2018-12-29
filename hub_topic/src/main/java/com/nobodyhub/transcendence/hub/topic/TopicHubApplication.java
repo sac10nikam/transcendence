d72c3e2dcef40842b610389167337defc6bd5cec
yan_h <yan_h@hotmail.com>
2018-12-29 21:29:41 +0800

    [hub_topic]create new module

---
+++ hub_topic/src/main/java/com/nobodyhub/transcendence/hub/topic/TopicHubApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.hub.topic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicHubApplication.class, args);
+    }
+
+}
+
