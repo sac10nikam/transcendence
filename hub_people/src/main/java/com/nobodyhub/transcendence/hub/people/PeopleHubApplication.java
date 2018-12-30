9b97a748b06af811a831eccca0414f9ebed415aa
yan_h <yan_h@hotmail.com>
2018-12-30 21:13:30 +0800

    [hub_people]create new module

---
+++ hub_people/src/main/java/com/nobodyhub/transcendence/hub/people/PeopleHubApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.hub.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleHubApplication.class, args);
+    }
+
+}
+
