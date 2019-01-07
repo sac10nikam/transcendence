38ad10c9d55f40cbc35bbb997bdc2bf2008f019e
yan_h <yan_h@hotmail.com>
2019-01-07 23:26:18 +0800

    [zhihu_api_throttle]add placeholder for ApiChannelBaseService

--- zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiThrottleService.java
+++ zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiThrottleService.java
@@ -1,13 +1,56 @@
 package com.nobodyhub.transcendence.zhihu.throttle.service;
 
+import com.nobodyhub.transcendence.api.common.converter.ApiResponseConverter;
+import com.nobodyhub.transcendence.api.common.cookies.ApiCookies;
+import com.nobodyhub.transcendence.api.common.executor.ApiAsyncExecutor;
+import com.nobodyhub.transcendence.api.common.kafka.KafkaHeaderHandler;
 import com.nobodyhub.transcendence.api.common.message.ApiChannelBaseService;
+import com.nobodyhub.transcendence.api.common.message.ApiRequestMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.binder.PollableMessageSource;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @EnableBinding(ZhihuApiChannel.class)
 public class ZhihuApiThrottleService extends ApiChannelBaseService<ZhihuApiChannel> {
+    private final ApiCookies cookies;
 
+
+    protected ZhihuApiThrottleService(ZhihuApiChannel channel,
+                                      ApiResponseConverter converter,
+                                      ApiAsyncExecutor apiAsyncExecutor,
+                                      KafkaHeaderHandler headerHandler,
+                                      PollableMessageSource requestMessageSource,
+                                      ApiCookies cookies) {
+        super(channel, converter, apiAsyncExecutor, headerHandler, requestMessageSource);
+        this.cookies = cookies;
+    }
+
+    protected final void makeOutboundRequest(PollableMessageSource requestMessageSource) {
+        try {
+            requestMessageSource.poll(m -> {
+                    ApiRequestMessage message = ((ApiRequestMessage) m.getPayload());
+                    apiAsyncExecutor.execRequest(message);
+                    // append the latest cookies
+                    injectCookies(message);
+                },
+                new ParameterizedTypeReference<ApiRequestMessage>() {
+                });
+        } catch (Exception e) {
+            log.error("Error happen when polling message.", e);
+        }
+    }
+
+    @Override
+    protected void injectCookies(ApiRequestMessage message) {
+        cookies.inject(message);
+    }
+
+    @Override
+    protected long getDelay() {
+        return 0;
+    }
 }
