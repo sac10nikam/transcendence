ad26b7a5fcd6b65abcc79689155a1ec6d9fd51d5
yan_h <yan_h@hotmail.com>
2019-01-07 23:25:13 +0800

    [api_common]add ApiCooklies using Redis

---
+++ api_common/src/main/java/com/nobodyhub/transcendence/api/common/cookies/ApiRedisConfiguration.java
@@ -0,0 +1,21 @@
+package com.nobodyhub.transcendence.api.common.cookies;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class ApiRedisConfiguration {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+    }
+
+    @Bean
+    public StringRedisTemplate redisTemplate() {
+        StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory());
+        return template;
+    }
+}
