48eee9e25ad4ddce715d2c801044c455843a89d5
yan_h <yan_h@hotmail.com>
2019-01-08 22:09:26 +0800

    [zhihu_api_throttle]add ApiChannel and Throttle Service

--- zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiChannel.java
+++ zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiChannel.java
@@ -1,6 +1,66 @@
 package com.nobodyhub.transcendence.zhihu.throttle.service;
 
 import com.nobodyhub.transcendence.api.common.message.ApiChannel;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.binder.PollableMessageSource;
 
+/**
+ * If the API share the thresholde, should send to the same channel
+ */
 public interface ZhihuApiChannel extends ApiChannel {
+    /**
+     * Request for Zhihu Answer
+     */
+    String ZHIHU_ANSWER_REQUEST_CHANNEL = "zhihu-answer-request-channel";
+
+    @Input(ZHIHU_ANSWER_REQUEST_CHANNEL)
+    PollableMessageSource receiveAnswerRequest();
+
+    /**
+     * Request for Zhihu Article
+     */
+    String ZHIHU_ARTICLE_REQUEST_CHANNEL = "zhihu-article-request-channel";
+
+    @Input(ZHIHU_ARTICLE_REQUEST_CHANNEL)
+    PollableMessageSource receiveArticleRequest();
+
+    /**
+     * Request for Zhihu Column
+     */
+    String ZHIHU_COLUMN_REQUEST_CHANNEL = "zhihu-column-request-channel";
+
+    @Input(ZHIHU_COLUMN_REQUEST_CHANNEL)
+    PollableMessageSource receiveColumnRequest();
+
+    /**
+     * Request for Zhihu Comment
+     */
+    String ZHIHU_COMMENT_REQUEST_CHANNEL = "zhihu-comment-request-channel";
+
+    @Input(ZHIHU_COMMENT_REQUEST_CHANNEL)
+    PollableMessageSource receiveCommentRequest();
+
+    /**
+     * Request for Zhihu Member
+     */
+    String ZHIHU_MEMBER_REQUEST_CHANNEL = "zhihu-member-request-channel";
+
+    @Input(ZHIHU_MEMBER_REQUEST_CHANNEL)
+    PollableMessageSource receiveMemberRequest();
+
+    /**
+     * Request for Zhihu Question
+     */
+    String ZHIHU_QUESTION_REQUEST_CHANNEL = "zhihu-question-request-channel";
+
+    @Input(ZHIHU_QUESTION_REQUEST_CHANNEL)
+    PollableMessageSource receiveQuestionRequest();
+
+    /**
+     * Request for Zhihu Topic
+     */
+    String ZHIHU_TOPIC_REQUEST_CHANNEL = "zhihu-topic-request-channel";
+
+    @Input(ZHIHU_TOPIC_REQUEST_CHANNEL)
+    PollableMessageSource receiveTopicRequest();
 }
--- zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiThrottleService.java
+++ zhihu_api_throttle/src/main/java/com/nobodyhub/transcendence/zhihu/throttle/service/ZhihuApiThrottleService.java
@@ -7,26 +7,85 @@ import com.nobodyhub.transcendence.api.common.kafka.KafkaHeaderHandler;
 import com.nobodyhub.transcendence.api.common.message.ApiChannelBaseService;
 import com.nobodyhub.transcendence.api.common.message.ApiRequestMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.binder.PollableMessageSource;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import static com.nobodyhub.transcendence.zhihu.throttle.service.ZhihuApiChannel.*;
 
 @Slf4j
 @Service
 @EnableBinding(ZhihuApiChannel.class)
 public class ZhihuApiThrottleService extends ApiChannelBaseService<ZhihuApiChannel> {
     private final ApiCookies cookies;
+    private final PollableMessageSource answerRequestSource;
+    private final PollableMessageSource articleRequestSource;
+    private final PollableMessageSource columnRequestSource;
+    private final PollableMessageSource commentRequestSource;
+    private final PollableMessageSource memberRequestSource;
+    private final PollableMessageSource questionRequestSource;
+    private final PollableMessageSource topicRequestSource;
 
 
     protected ZhihuApiThrottleService(ZhihuApiChannel channel,
                                       ApiResponseConverter converter,
                                       ApiAsyncExecutor apiAsyncExecutor,
                                       KafkaHeaderHandler headerHandler,
-                                      PollableMessageSource requestMessageSource,
-                                      ApiCookies cookies) {
-        super(channel, converter, apiAsyncExecutor, headerHandler, requestMessageSource);
+                                      ApiCookies cookies,
+                                      @Qualifier(ZHIHU_ANSWER_REQUEST_CHANNEL) PollableMessageSource answerRequestSource,
+                                      @Qualifier(ZHIHU_ARTICLE_REQUEST_CHANNEL) PollableMessageSource articleRequestSource,
+                                      @Qualifier(ZHIHU_COLUMN_REQUEST_CHANNEL) PollableMessageSource columnRequestSource,
+                                      @Qualifier(ZHIHU_COMMENT_REQUEST_CHANNEL) PollableMessageSource commentRequestSource,
+                                      @Qualifier(ZHIHU_MEMBER_REQUEST_CHANNEL) PollableMessageSource memberRequestSource,
+                                      @Qualifier(ZHIHU_QUESTION_REQUEST_CHANNEL) PollableMessageSource questionRequestSource,
+                                      @Qualifier(ZHIHU_TOPIC_REQUEST_CHANNEL) PollableMessageSource topicRequestSource) {
+        super(channel, converter, apiAsyncExecutor, headerHandler);
         this.cookies = cookies;
+        this.answerRequestSource = answerRequestSource;
+        this.articleRequestSource = articleRequestSource;
+        this.columnRequestSource = columnRequestSource;
+        this.commentRequestSource = commentRequestSource;
+        this.memberRequestSource = memberRequestSource;
+        this.questionRequestSource = questionRequestSource;
+        this.topicRequestSource = topicRequestSource;
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveAnswerRequest() {
+        makeOutboundRequest(answerRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveArticleRequest() {
+        makeOutboundRequest(articleRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveColumnRequest() {
+        makeOutboundRequest(columnRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveCommentRequest() {
+        makeOutboundRequest(commentRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveMemberRequest() {
+        makeOutboundRequest(memberRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveQuestionRequest() {
+        makeOutboundRequest(questionRequestSource);
+    }
+
+    @Scheduled(fixedDelayString = "${zhihu.api.delay}")
+    public void receiveTopicRequest() {
+        makeOutboundRequest(topicRequestSource);
     }
 
     protected final void makeOutboundRequest(PollableMessageSource requestMessageSource) {
@@ -35,22 +94,12 @@ public class ZhihuApiThrottleService extends ApiChannelBaseService<ZhihuApiChann
                     ApiRequestMessage message = ((ApiRequestMessage) m.getPayload());
                     apiAsyncExecutor.execRequest(message);
                     // append the latest cookies
-                    injectCookies(message);
+                    cookies.inject(message);
                 },
                 new ParameterizedTypeReference<ApiRequestMessage>() {
                 });
         } catch (Exception e) {
             log.error("Error happen when polling message.", e);
         }
-    }
-
-    @Override
-    protected void injectCookies(ApiRequestMessage message) {
-        cookies.inject(message);
-    }
-
-    @Override
-    protected long getDelay() {
-        return 0;
     }
 }
