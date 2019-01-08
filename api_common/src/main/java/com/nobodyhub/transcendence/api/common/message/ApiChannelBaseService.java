2ad0f5cb76f92a75cbd5a34c3465be767d4b5254
yan_h <yan_h@hotmail.com>
2019-01-08 22:06:00 +0800

    [api_common]remove throttle related function/member

--- api_common/src/main/java/com/nobodyhub/transcendence/api/common/message/ApiChannelBaseService.java
+++ api_common/src/main/java/com/nobodyhub/transcendence/api/common/message/ApiChannelBaseService.java
@@ -4,8 +4,6 @@ import com.nobodyhub.transcendence.api.common.converter.ApiResponseConverter;
 import com.nobodyhub.transcendence.api.common.executor.ApiAsyncExecutor;
 import com.nobodyhub.transcendence.api.common.kafka.KafkaHeaderHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.binder.PollableMessageSource;
-import org.springframework.core.ParameterizedTypeReference;
 
 @Slf4j
 public abstract class ApiChannelBaseService<T extends ApiChannel> {
@@ -13,46 +11,14 @@ public abstract class ApiChannelBaseService<T extends ApiChannel> {
     protected final ApiResponseConverter converter;
     protected final ApiAsyncExecutor apiAsyncExecutor;
     protected final KafkaHeaderHandler headerHandler;
-    protected final PollableMessageSource requestMessageSource;
 
     protected ApiChannelBaseService(T channel,
                                     ApiResponseConverter converter,
                                     ApiAsyncExecutor apiAsyncExecutor,
-                                    KafkaHeaderHandler headerHandler,
-                                    PollableMessageSource requestMessageSource) {
+                                    KafkaHeaderHandler headerHandler) {
         this.channel = channel;
         this.converter = converter;
         this.apiAsyncExecutor = apiAsyncExecutor;
         this.headerHandler = headerHandler;
-        this.requestMessageSource = requestMessageSource;
     }
-
-    protected final void makeOutboundRequest() {
-        try {
-            requestMessageSource.poll(m -> {
-                    ApiRequestMessage message = ((ApiRequestMessage) m.getPayload());
-                    apiAsyncExecutor.execRequest(message);
-                    // append the latest cookies
-                    injectCookies(message);
-                },
-                new ParameterizedTypeReference<ApiRequestMessage>() {
-                });
-        } catch (Exception e) {
-            log.error("Error happen when polling message.", e);
-        }
-    }
-
-    /**
-     * Inject cookies into the request message
-     *
-     * @param message request message
-     */
-    protected abstract void injectCookies(ApiRequestMessage message);
-
-    /**
-     * Get the interval in millisecond between each request
-     *
-     * @return
-     */
-    protected abstract long getDelay();
 }
