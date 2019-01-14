c75c7444830483a45fe2a8ee7094defc66fcb63a
yan_h <yan_h@hotmail.com>
2019-01-14 22:24:39 +0800

    [common_util]add TopicData#getId

--- common_utils/src/main/java/com/nobodyhub/transcendence/common/domain/TopicData.java
+++ common_utils/src/main/java/com/nobodyhub/transcendence/common/domain/TopicData.java
@@ -4,4 +4,10 @@ package com.nobodyhub.transcendence.common.domain;
  * Mark class that corresponds with certern topic type
  */
 public interface TopicData {
+    /**
+     * Get the unique id of data
+     *
+     * @return
+     */
+    String getId();
 }
