bc2b0a80153b06ff92a43ff244fa80bab1dd2d3b
yan_h <yan_h@hotmail.com>
2019-01-24 22:47:31 +0800

    [message_controll]create module

---
+++ message_controll/src/main/java/com/nobodyhub/transcendence/message/controll/MessageControllApplication.java
@@ -0,0 +1,14 @@
+package com.nobodyhub.transcendence.message.controll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageControllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageControllApplication.class, args);
+    }
+
+}
+
---
+++ message_controll/src/test/java/com/nobodyhub/transcendence/message/controll/MessageControllApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nobodyhub.transcendence.message.controll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageControllApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
