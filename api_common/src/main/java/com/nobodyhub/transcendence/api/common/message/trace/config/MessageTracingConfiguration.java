91e7a6da8eca22d6de41fb4770c80535664a3d97
yan_h <yan_h@hotmail.com>
2019-01-30 23:05:50 +0800

    [api_common]add redis support

--- api_common/src/main/java/com/nobodyhub/transcendence/api/common/message/trace/config/MessageTracingConfiguration.java
+++ api_common/src/main/java/com/nobodyhub/transcendence/api/common/message/trace/config/MessageTracingConfiguration.java
@@ -3,6 +3,9 @@ package com.nobodyhub.transcendence.api.common.message.trace.config;
 import com.nobodyhub.transcendence.api.common.message.trace.aop.MessageTraceAspect;
 import com.nobodyhub.transcendence.api.common.message.trace.extractor.TraceTargetExtractorManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * Configuration for message tracing
@@ -17,5 +20,15 @@ public class MessageTracingConfiguration {
     @Bean
     public TraceTargetExtractorManager messageExtractorManage() {
         return new TraceTargetExtractorManager();
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("javis-server", 6379));
+    }
+
+    @Bean
+    public StringRedisTemplate redisTemplate() {
+        return new StringRedisTemplate(redisConnectionFactory());
     }
 }
